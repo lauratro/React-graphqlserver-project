515848947dc0a021687cf4c62e9c0655113286e2
Laura <65158950+lauratro@users.noreply.github.com>
2021-10-15 10:32:05 +0200

    db file created

--- server/index.js
+++ server/index.js
@@ -1,1 +1,34 @@
 const { ApolloServer, gql } = require("apollo-server");
+const { mainCards, animals } = require("./db");
+const typeDefs = gql`
+  type MainCard {
+    title: String
+    image: String
+  }
+  type Animal {
+    image: String!
+    title: String!
+    rating: Float
+    price: String!
+    description: [String!]!
+    stock: Int!
+    onSale: Boolean
+  }
+  type Query {
+    mainCards: [MainCard]
+    animals: [Animal]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    mainCards: () => mainCards,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
